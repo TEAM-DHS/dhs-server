386d3560fda2ce7ed27d945b91e478037a86553f
Kwon Min A <min2010011@gmail.com>
2023-10-02 01:46:46 +0900

    Init project

---
+++ src/test/java/com/efub/dhs/DhsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.efub.dhs;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DhsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
