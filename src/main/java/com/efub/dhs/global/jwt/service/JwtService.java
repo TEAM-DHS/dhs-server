ec06b0fb91cb0058990fb5829a8e3989d5481d49
Kwon Min A <min2010011@gmail.com>
2023-11-07 14:21:42 +0900

    feat: 액세스 토큰 재발급 서비스 메소드 추가 #5

--- src/main/java/com/efub/dhs/global/jwt/service/JwtService.java
+++ src/main/java/com/efub/dhs/global/jwt/service/JwtService.java
@@ -2,6 +2,7 @@ package com.efub.dhs.global.jwt.service;
 
 import org.springframework.stereotype.Service;
 
+import com.efub.dhs.global.jwt.auth.JwtAuthProvider;
 import com.efub.dhs.global.jwt.entity.JwtToken;
 import com.efub.dhs.global.jwt.repository.JwtRepository;
 
@@ -12,14 +13,27 @@ import lombok.RequiredArgsConstructor;
 public class JwtService {
 
 	private final JwtRepository jwtRepository;
+	private final JwtAuthProvider jwtAuthProvider;
 
 	public JwtToken saveJwtToken(JwtToken jwtToken) {
 		return jwtRepository.save(jwtToken);
 	}
 
 	public void removeJwtToken(String accessToken) {
-		JwtToken jwtToken = jwtRepository.findByAccessToken(accessToken)
-			.orElseThrow(() -> new SecurityException("JWT token is invalid."));
+		JwtToken jwtToken = getJwtToken(accessToken);
 		jwtRepository.delete(jwtToken);
+	}
+
+	public JwtToken refreshToken(String accessToken) {
+		JwtToken jwtToken = getJwtToken(accessToken);
+		String reissuedAccessToken = jwtAuthProvider.reissueAccessToken(jwtToken);
+		jwtToken.updateAccessToken(reissuedAccessToken);
+		saveJwtToken(jwtToken);
+		return jwtToken;
+	}
+
+	private JwtToken getJwtToken(String accessToken) {
+		return jwtRepository.findByAccessToken(accessToken)
+			.orElseThrow(() -> new SecurityException("JWT token is invalid."));
 	}
 }
