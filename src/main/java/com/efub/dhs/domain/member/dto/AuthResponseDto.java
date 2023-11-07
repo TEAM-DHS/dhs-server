426d5f34eb04c18abf922b461082b577a2f5f4f4
Kwon Min A <min2010011@gmail.com>
2023-11-07 14:21:26 +0900

    rename: LogInResponseDto -> AuthResponseDto 이름 변경 #5

--- src/main/java/com/efub/dhs/domain/member/dto/AuthResponseDto.java
+++ src/main/java/com/efub/dhs/domain/member/dto/AuthResponseDto.java
@@ -5,7 +5,7 @@ import lombok.Getter;
 
 @Getter
 @AllArgsConstructor
-public class LogInResponseDto {
+public class AuthResponseDto {
 
 	private final String grantType;
 	private final String accessToken;
