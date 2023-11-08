e9d915d767dcfaa85900695d1baab17b34ad1c3a
Kwon Min A <min2010011@gmail.com>
2023-11-08 18:22:06 +0900

    feat: 카카오 토큰 정보 요청을 위한 Feign Client 추가 #10

---
+++ src/main/java/com/efub/dhs/global/feign/client/KakaoUserInfoClient.java
@@ -0,0 +1,15 @@
+package com.efub.dhs.global.feign.client;
+
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.HttpHeaders;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
+
+import com.efub.dhs.global.feign.dto.response.KakaoUserInfoResponseDto;
+
+@FeignClient(name = "KakaoUserInfoClient", url = "https://kapi.kakao.com/v1/user/access_token_info")
+public interface KakaoUserInfoClient {
+
+	@GetMapping
+	KakaoUserInfoResponseDto getUserInfo(@RequestHeader HttpHeaders headers);
+}
