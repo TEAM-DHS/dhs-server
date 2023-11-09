b63baca0163e37fab30433d7634978ce6fb4ad8b
Kwon Min A <min2010011@gmail.com>
2023-11-09 15:25:18 +0900

    refactor: Content-type 설정을 문자열에서 스프링 상수로 변경 #10

--- src/main/java/com/efub/dhs/global/feign/client/KakaoTokenClient.java
+++ src/main/java/com/efub/dhs/global/feign/client/KakaoTokenClient.java
@@ -1,14 +1,16 @@
 package com.efub.dhs.global.feign.client;
 
 import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import com.efub.dhs.global.feign.dto.request.KakaoTokenRequestDto;
 import com.efub.dhs.global.feign.dto.response.KakaoTokenResponseDto;
 
-@FeignClient(name = "KakaoTokenClient", url = "https://kauth.kakao.com/oauth/token")
+@FeignClient(name = "kakaoOAuth", url = "https://kauth.kakao.com/oauth/token")
 public interface KakaoTokenClient {
 
-	@PostMapping(consumes = "application/x-www-form-urlencoded")
-	KakaoTokenResponseDto getToken(KakaoTokenRequestDto requestDto);
+	@PostMapping(consumes = MediaType.APPLICATION_FORM_URLENCODED_VALUE)
+	KakaoTokenResponseDto getToken(@RequestBody KakaoTokenRequestDto requestDto);
 }
