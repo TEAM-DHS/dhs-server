941dda7945bb58b435a9900c2b65ec14e4b137ea
Kwon Min A <min2010011@gmail.com>
2023-11-16 22:57:16 +0900

    feat: OAuth 회원은 별도로 생성한 Authentication으로 JWT를 발급하도록 변경 #24

--- src/main/java/com/efub/dhs/domain/member/service/KakaoOAuthService.java
+++ src/main/java/com/efub/dhs/domain/member/service/KakaoOAuthService.java
@@ -6,7 +6,9 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpHeaders;
-import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.authority.AuthorityUtils;
 import org.springframework.stereotype.Service;
 
 import com.efub.dhs.domain.member.entity.Member;
@@ -16,6 +18,7 @@ import com.efub.dhs.global.feign.client.KakaoUserInfoClient;
 import com.efub.dhs.global.feign.dto.request.KakaoTokenRequestDto;
 import com.efub.dhs.global.feign.dto.response.KakaoTokenResponseDto;
 import com.efub.dhs.global.feign.dto.response.KakaoUserInfoResponseDto;
+import com.efub.dhs.global.jwt.auth.JwtAuthProvider;
 import com.efub.dhs.global.jwt.entity.JwtToken;
 
 import lombok.RequiredArgsConstructor;
@@ -30,20 +33,19 @@ public class KakaoOAuthService {
 	private final KakaoUserInfoClient kakaoUserInfoClient;
 	private final MemberRepository memberRepository;
 	private final AuthService authService;
-	private final PasswordEncoder passwordEncoder;
+	private final JwtAuthProvider jwtAuthProvider;
 
 	@Value("${kakao.client-id}")
 	private String clientId;
-
-	@Value("${kakao.credentials}")
-	private String kakaoCredentials;
 
 	public JwtToken signUp(String redirectUri, String code) {
 		String kakaoToken = getKakaoToken(redirectUri, code);
 		String username = getKakaoUsername(kakaoToken);
 		log.debug("[KAKAO] 회원 ID: {}", username);
 		createMember(username);
-		return authService.logIn(username, kakaoCredentials);
+		Authentication authentication = new UsernamePasswordAuthenticationToken(
+			username, null, AuthorityUtils.createAuthorityList("ROLE_USER"));
+		return jwtAuthProvider.generateJwtToken(authentication);
 	}
 
 	private String getKakaoToken(String redirectUri, String code) {
@@ -70,8 +72,8 @@ public class KakaoOAuthService {
 		if (optionalMember.isEmpty()) {
 			Member member = Member.builder()
 				.username(username)
-				.password(passwordEncoder.encode(kakaoCredentials))
 				.build();
+			member.convertToOAuthUser();
 			memberRepository.save(member);
 		}
 	}
