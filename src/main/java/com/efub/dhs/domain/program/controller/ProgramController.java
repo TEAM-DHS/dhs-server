3c5bda907d834c90831b04eb7b429bd150ddbb8c
Kwon Min A <min2010011@gmail.com>
2023-11-16 15:04:58 +0900

    feat: 프로그램 등록 컨트롤러 메소드 추가 #24

--- src/main/java/com/efub/dhs/domain/program/controller/ProgramController.java
+++ src/main/java/com/efub/dhs/domain/program/controller/ProgramController.java
@@ -3,11 +3,15 @@ package com.efub.dhs.domain.program.controller;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.efub.dhs.domain.program.dto.ProgramDetailResponseDto;
+import com.efub.dhs.domain.program.dto.ProgramRegistryRequestDto;
+import com.efub.dhs.domain.program.dto.ProgramRegistryResponseDto;
 import com.efub.dhs.domain.program.service.ProgramService;
 
 import lombok.RequiredArgsConstructor;
@@ -23,5 +27,11 @@ public class ProgramController {
 	@ResponseStatus(value = HttpStatus.OK)
 	public ProgramDetailResponseDto programFind(@PathVariable Long programId) {
 		return programService.findProgramById(programId);
+	}
+
+	@PostMapping
+	@ResponseStatus(value = HttpStatus.CREATED)
+	public ProgramRegistryResponseDto registerProgram(@RequestBody ProgramRegistryRequestDto requestDto) {
+		return new ProgramRegistryResponseDto(programService.registerProgram(requestDto));
 	}
 }
