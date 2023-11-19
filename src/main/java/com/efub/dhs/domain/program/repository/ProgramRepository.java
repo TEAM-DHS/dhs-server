01658c3ae9f9edfa0aa366899c06c917af7589aa
Kwon Min A <min2010011@gmail.com>
2023-11-20 03:04:57 +0900

    feat: 좋아요한 행사 리스트 조회 쿼리 및 메소드 추가 #31

--- src/main/java/com/efub/dhs/domain/program/repository/ProgramRepository.java
+++ src/main/java/com/efub/dhs/domain/program/repository/ProgramRepository.java
@@ -2,8 +2,12 @@ package com.efub.dhs.domain.program.repository;
 
 import java.util.List;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import com.efub.dhs.domain.member.entity.Member;
 import com.efub.dhs.domain.program.entity.Category;
 import com.efub.dhs.domain.program.entity.Program;
 
@@ -11,4 +15,9 @@ public interface ProgramRepository extends JpaRepository<Program, Long> {
 
 	//List<Program> findTop3ByCategoryAndScheduleMonth(Category category, Month month);
 	List<Program> findTop3ByCategory(Category category);
+
+	Page<Program> findAllByHost(Member host, Pageable pageable);
+
+	@Query(value = "select p from Program p join fetch Heart h on h.program=p where h.member=?1")
+	Page<Program> findAllProgramLiked(Member member, Pageable pageable);
 }
