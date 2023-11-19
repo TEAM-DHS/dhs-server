22d37e85fec523b7ecc733ae6c8cb9a8e3fb5ef1
Kwon Min A <min2010011@gmail.com>
2023-11-20 03:06:23 +0900

    feat: 좋아요한 행사 리스트 조회를 위한 서비스 메소드 추가 #31

---
+++ src/main/java/com/efub/dhs/domain/program/service/ProgramMemberService.java
@@ -0,0 +1,37 @@
+package com.efub.dhs.domain.program.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.efub.dhs.domain.member.entity.Member;
+import com.efub.dhs.domain.program.dto.PageInfoDto;
+import com.efub.dhs.domain.program.dto.response.ProgramLikedResponseDto;
+import com.efub.dhs.domain.program.dto.response.ProgramOutlineResponseDto;
+import com.efub.dhs.domain.program.entity.Program;
+import com.efub.dhs.domain.program.repository.ProgramRepository;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class ProgramMemberService {
+
+	private static final int PAGE_SIZE = 12;
+
+	private final ProgramRepository programRepository;
+	private final ProgramService programService;
+
+	@Transactional(readOnly = true)
+	public ProgramLikedResponseDto findProgramLiked(int page) {
+		Member currentUser = programService.getCurrentUser();
+		Page<Program> programPage = programRepository.findAllProgramLiked(currentUser, PageRequest.of(page, PAGE_SIZE));
+		PageInfoDto pageInfoDto = PageInfoDto.from(programPage);
+		List<ProgramOutlineResponseDto> programOutlineResponseDtoList =
+			programService.convertToProgramOutlineResponseDtoList(programPage.getContent(), currentUser);
+		return new ProgramLikedResponseDto(programOutlineResponseDtoList, pageInfoDto);
+	}
+}
