63c88e18df0aa7a8d6a182a40b3c843cfc63bf3d
Kwon Min A <min2010011@gmail.com>
2023-11-21 01:15:29 +0900

    feat: 등록한 행사 조회 API 컨트롤러 메소드 추가 #17

--- src/main/java/com/efub/dhs/domain/program/controller/ProgramController.java
+++ src/main/java/com/efub/dhs/domain/program/controller/ProgramController.java
@@ -14,6 +14,7 @@ import org.springframework.web.bind.annotation.RestController;
 
 import com.efub.dhs.domain.program.dto.request.ProgramCreationRequestDto;
 import com.efub.dhs.domain.program.dto.request.ProgramRegistrationRequestDto;
+import com.efub.dhs.domain.program.dto.response.ProgramCreatedResponseDto;
 import com.efub.dhs.domain.program.dto.response.ProgramCreationResponseDto;
 import com.efub.dhs.domain.program.dto.response.ProgramDetailResponseDto;
 import com.efub.dhs.domain.program.dto.response.ProgramLikedResponseDto;
@@ -50,6 +51,11 @@ public class ProgramController {
 		@RequestBody @Valid ProgramRegistrationRequestDto requestDto) {
 		Registration savedRegistration = programService.registerProgram(programId, requestDto);
 		return ProgramRegistrationResponseDto.from(savedRegistration);
+	}
+
+	@GetMapping("/created")
+	public ProgramCreatedResponseDto findProgramCreated(@RequestParam int page) {
+		return programMemberService.findProgramCreated(page);
 	}
 
 	@GetMapping("/liked")
