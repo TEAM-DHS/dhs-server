add3ed3758040a10defcc33c0524f35851f1884c
Kwon Min A <min2010011@gmail.com>
2023-11-23 02:01:46 +0900

    rename: RefundStatus 필드명 REQUIRED -> REQUESTED 변경 #19

--- src/main/java/com/efub/dhs/domain/registration/entity/RefundStatus.java
+++ src/main/java/com/efub/dhs/domain/registration/entity/RefundStatus.java
@@ -5,7 +5,7 @@ import lombok.RequiredArgsConstructor;
 @RequiredArgsConstructor
 public enum RefundStatus {
 	NONE("해당없음"),
-	REQUIRED("환불필요"),
+	REQUESTED("환불요청"),
 	COMPLETED("환불완료");
 
 	private final String name;
