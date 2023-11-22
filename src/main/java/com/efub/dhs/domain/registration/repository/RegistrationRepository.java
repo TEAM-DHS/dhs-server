ae78df57a96894c8867ba7920f44cc5fe4aab938
Kwon Min A <min2010011@gmail.com>
2023-11-23 02:02:18 +0900

    feat: 행사 신청 정보 조회를 위한 레포지토리 메소드 추가 #19

--- src/main/java/com/efub/dhs/domain/registration/repository/RegistrationRepository.java
+++ src/main/java/com/efub/dhs/domain/registration/repository/RegistrationRepository.java
@@ -1,6 +1,9 @@
 package com.efub.dhs.domain.registration.repository;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import com.efub.dhs.domain.member.entity.Member;
 import com.efub.dhs.domain.program.entity.Program;
@@ -9,4 +12,7 @@ import com.efub.dhs.domain.registration.entity.Registration;
 public interface RegistrationRepository extends JpaRepository<Registration, Long> {
 
 	Boolean existsByMemberAndProgram(Member member, Program program);
+
+	@Query(value = "select r from Registration r join fetch Program p on r.program=p where r.member=?1")
+	Page<Registration> findRegisteredPrograms(Member registrant, Pageable pageable);
 }
