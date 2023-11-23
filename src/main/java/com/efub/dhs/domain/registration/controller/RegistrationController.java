fbc9f7bdd9f9e4f23980d08747349f39148356cf
Kwon Min A <min2010011@gmail.com>
2023-11-24 02:19:45 +0900

    feat: 신청 정보 수정을 위한 컨트롤러 메소드 추가 #44

--- src/main/java/com/efub/dhs/domain/registration/controller/RegistrationController.java
+++ src/main/java/com/efub/dhs/domain/registration/controller/RegistrationController.java
@@ -1,11 +1,14 @@
 package com.efub.dhs.domain.registration.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.efub.dhs.domain.program.dto.response.ProgramRegistrationResponseDto;
+import com.efub.dhs.domain.registration.dto.RegistrationModificationRequestDto;
 import com.efub.dhs.domain.registration.entity.Registration;
 import com.efub.dhs.domain.registration.service.RegistrationService;
 
@@ -22,5 +25,12 @@ public class RegistrationController {
 	public ProgramRegistrationResponseDto findRegistration(@PathVariable Long registrationId) {
 		Registration registration = registrationService.findRegistration(registrationId);
 		return ProgramRegistrationResponseDto.from(registration);
+	}
+
+	@PatchMapping("/{registrationId}")
+	public ProgramRegistrationResponseDto modifyRegistration(
+		@PathVariable Long registrationId, @RequestBody RegistrationModificationRequestDto requestDto) {
+		Registration modifiedRegistration = registrationService.modifyRegistration(registrationId, requestDto);
+		return ProgramRegistrationResponseDto.from(modifiedRegistration);
 	}
 }
