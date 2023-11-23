80cb82097acf41ff347477d81fdc8be42b60268e
yeji <yeji.lee.kim@gmail.com>
2023-11-23 23:34:09 +0900

    feat: 신청자 정보 조회를 위한 서비스 메소드 추가

--- src/main/java/com/efub/dhs/domain/registration/service/RegistrationService.java
+++ src/main/java/com/efub/dhs/domain/registration/service/RegistrationService.java
@@ -1,10 +1,15 @@
 package com.efub.dhs.domain.registration.service;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
 import com.efub.dhs.domain.member.entity.Member;
+import com.efub.dhs.domain.member.service.MemberService;
 import com.efub.dhs.domain.program.entity.Program;
+import com.efub.dhs.domain.registration.dto.RegistrationResponseDto;
 import com.efub.dhs.domain.registration.entity.Registration;
 import com.efub.dhs.domain.registration.repository.RegistrationRepository;
 
@@ -16,6 +21,7 @@ import lombok.RequiredArgsConstructor;
 public class RegistrationService {
 
 	private final RegistrationRepository registrationRepository;
+	private final MemberService memberService;
 
 	@Transactional(readOnly = true)
 	public Boolean existsByMemberAndProgram(Member member, Program program) {
@@ -25,5 +31,16 @@ public class RegistrationService {
 	@Transactional(readOnly = true)
 	public Registration saveRegistration(Registration registration) {
 		return registrationRepository.save(registration);
+	}
+
+	@Transactional(readOnly = true)
+	public List<RegistrationResponseDto> findRegistratorList(Program program) {
+		Member member = memberService.getCurrentUser();
+		if (member.equals(program.getHost())) {
+			List<Registration> registrationList = registrationRepository.findAllByProgram(program);
+			return registrationList.stream().map(RegistrationResponseDto::from).collect(Collectors.toList());
+		} else {
+			throw new SecurityException("접근 권한이 없습니다.");
+		}
 	}
 }
