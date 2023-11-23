d50698cbcadf9e5088023d511b3f43dc71c70f5b
Kwon Min A <min2010011@gmail.com>
2023-11-24 01:41:14 +0900

    feat: 행사 참가 신청 API 응답 DTO 수정 #44

--- src/main/java/com/efub/dhs/domain/program/dto/response/ProgramRegistrationResponseDto.java
+++ src/main/java/com/efub/dhs/domain/program/dto/response/ProgramRegistrationResponseDto.java
@@ -1,32 +1,43 @@
 package com.efub.dhs.domain.program.dto.response;
 
-import com.efub.dhs.domain.program.dto.request.ProgramRegistrationRequestDto;
+import java.time.LocalDateTime;
+
+import com.efub.dhs.domain.registration.entity.RefundStatus;
 import com.efub.dhs.domain.registration.entity.Registration;
 
 import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Getter;
 
 @Getter
+@Builder
 @AllArgsConstructor(access = AccessLevel.PROTECTED)
 public class ProgramRegistrationResponseDto {
 
 	private long registrationId;
-	private ProgramRegistrationRequestDto registrationInfo;
+	private String depositorName;
+	private String depositAmount;
+	private LocalDateTime depositDate;
+	private String registrantName;
+	private String registrantPhone;
+	private String refundBank;
+	private String refundAccount;
+	private String refundName;
+	private String refundStatus;
 
 	public static ProgramRegistrationResponseDto from(Registration registration) {
-		return new ProgramRegistrationResponseDto(
-			registration.getRegistrationId(),
-			ProgramRegistrationRequestDto.builder()
-				.depositorName(registration.getDepositName())
-				.depositAmount(registration.getDepositAmount())
-				.depositDate(registration.getDepositDate())
-				.registrantName(registration.getRegistrantName())
-				.registrantPhone(registration.getRegistrantPhone())
-				.refundBank(registration.getRefundBank())
-				.refundAccount(registration.getRefundAccount())
-				.refundName(registration.getRefundName())
-				.build()
-		);
+		return ProgramRegistrationResponseDto.builder()
+			.registrationId(registration.getRegistrationId())
+			.depositorName(registration.getDepositName())
+			.depositAmount(registration.getDepositAmount())
+			.depositDate(registration.getDepositDate())
+			.registrantName(registration.getRegistrantName())
+			.registrantPhone(registration.getRegistrantPhone())
+			.refundBank(registration.getRefundBank())
+			.refundAccount(registration.getRefundAccount())
+			.refundName(registration.getRefundName())
+			.refundStatus(RefundStatus.to(registration.getRefundStatus()))
+			.build();
 	}
 }
