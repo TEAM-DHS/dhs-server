585b529e7d52acfdf743c8437cfbc095c88c285f
Kwon Min A <min2010011@gmail.com>
2023-11-24 01:46:38 +0900

    feat: 신청한 행사 리스트 응답 DTO에 신청 정보를 포함 #44

--- src/main/java/com/efub/dhs/domain/program/dto/response/ProgramRegisteredResponseDto.java
+++ src/main/java/com/efub/dhs/domain/program/dto/response/ProgramRegisteredResponseDto.java
@@ -5,7 +5,7 @@ import java.util.List;
 import com.efub.dhs.domain.program.dto.GoalDto;
 import com.efub.dhs.domain.program.dto.PageInfoDto;
 import com.efub.dhs.domain.program.entity.Program;
-import com.efub.dhs.domain.registration.entity.RefundStatus;
+import com.efub.dhs.domain.registration.entity.Registration;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
@@ -14,17 +14,17 @@ import lombok.Getter;
 @AllArgsConstructor
 public class ProgramRegisteredResponseDto {
 
-	private List<ProgramOutlineWithStatusDto> programs;
+	private List<ProgramOutlineWithRegistrationDto> programs;
 	private PageInfoDto pageInfo;
 
 	@Getter
-	public static class ProgramOutlineWithStatusDto extends ProgramOutlineResponseDto {
-		private final RefundStatus refundStatus;
+	public static class ProgramOutlineWithRegistrationDto extends ProgramOutlineResponseDto {
+		private final ProgramRegistrationResponseDto registrationInfoDto;
 
-		public ProgramOutlineWithStatusDto(
-			Program program, Integer remainingDays, GoalDto goal, Boolean hasLike, RefundStatus refundStatus) {
+		public ProgramOutlineWithRegistrationDto(
+			Program program, Integer remainingDays, GoalDto goal, Boolean hasLike, Registration registration) {
 			super(program, remainingDays, goal, hasLike);
-			this.refundStatus = refundStatus;
+			this.registrationInfoDto = ProgramRegistrationResponseDto.from(registration);
 		}
 	}
 }
