a3a7f53f0c8e1d59679cea4e0251be850afc0b63
yeji <yeji.lee.kim@gmail.com>
2023-11-25 22:48:13 +0900

    fix: 인증 정보 찾지 못했을 경우 예외를 던지지 않고 null 리턴

--- src/main/java/com/efub/dhs/global/utils/SecurityUtils.java
+++ src/main/java/com/efub/dhs/global/utils/SecurityUtils.java
@@ -1,7 +1,6 @@
 package com.efub.dhs.global.utils;
 
 import org.springframework.security.core.Authentication;
-import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
 
 public class SecurityUtils {
@@ -9,8 +8,7 @@ public class SecurityUtils {
 	public static String getCurrentUsername() {
 		Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 		if (authentication == null || authentication.getName() == null) {
-			throw new AuthenticationException("There is no authentication information for the current user.") {
-			};
+			return null;
 		}
 		return authentication.getName();
 	}
